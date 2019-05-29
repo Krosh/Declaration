572b402a00f8f1fe6290fee85846829db00cc491
Pavel Prokhorov <eliom@inbox.ru>
2019-05-29 14:29:03 +0700

    * remove unused files

--- src/declaration/declaration.ts
+++ src/declaration/declaration.ts
@@ -3,13 +3,16 @@ import ValuesKeeper from './values-keeper'
 import PageKeeper from './page-keeper'
 
 export interface DataProvider {
-  saveAnswer: () => void
+  saveAnswer: (questionCode: string, id: number, value: string) => void
+  deleteMultiple: (questionCode: string, id: number) => void
+  copyMultiple: (questionCode: string, id: number, newId: number) => void
 }
 
 export default class Declaration {
   schema: FullyLoadedDeclaration
   valuesKeeper: ValuesKeeper
-  pagesKeeper: PageKeeper
+  private pagesKeeper: PageKeeper
+  dataProvider: DataProvider
 
   constructor(
     schema: FullyLoadedDeclaration,
@@ -19,5 +22,18 @@ export default class Declaration {
     this.schema = schema
     this.valuesKeeper = new ValuesKeeper(initialValues)
     this.pagesKeeper = new PageKeeper(schema, this.valuesKeeper.getValue)
+    this.dataProvider = dataProvider
   }
+
+  setActivePage = this.pagesKeeper.setActivePage
+  setActiveTab = this.pagesKeeper.setActiveTab
+
+  isActiveTab = this.pagesKeeper.isActiveTab
+  isActivePage = this.pagesKeeper.isActivePage
+
+  getActiveTab = this.pagesKeeper.getActiveTab
+  getActivePage = this.pagesKeeper.getActivePage
+
+  getVisibleTabs = () => this.pagesKeeper.tabs
+  getVisiblePages = () => this.pagesKeeper.visiblePages
 }
